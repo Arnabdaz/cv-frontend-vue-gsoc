41f1fecadb5c8447ba52b83819b6b9bf347da8db
Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>
2023-05-02 20:57:52 +0530

    fix(dragging): fix dragability of panels using interect.js library
    Signed-off-by: Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>

--- src/simulator/src/ux.js
+++ src/simulator/src/ux.js
@@ -25,6 +25,7 @@ import { setupVerilogExportCodeWindow } from './verilog'
 import { setupBitConvertor } from './utils'
 import { updateTestbenchUI, setupTestbenchUI } from './testbench'
 import { applyVerilogTheme } from './Verilog2CV'
+import interact from 'interactjs'
 
 export const uxvar = {
     smartDropXX: 50,
@@ -718,15 +719,103 @@ $('#octalInput').on('keyup', () => {
     setBaseValues(x)
 })
 
+/**
+ * Make an element draggable within a specified container.
+ * @param {string} targetEl - Selector for the element that triggers the drag event.
+ * @param {string} DragEl - Selector for the element to be dragged.
+ */
+function dragging(targetEl, DragEl) {
+    /**
+     * Helper function to update the position of an element.
+     * @param {HTMLElement} element - The element to be repositioned.
+     * @param {number} dx - Change in x-axis position.
+     * @param {number} dy - Change in y-axis position.
+     */
+    function updatePosition(element, dx, dy) {
+        if (!element) return // Check if the element is valid
+
+        // If the element does not exist in the positions WeakMap, create it
+        if (!positions.has(element)) {
+            positions.set(element, { x: 0, y: 0 })
+        }
+
+        // Update the element's x and y position
+        const currentPosition = positions.get(element)
+        currentPosition.x += dx
+        currentPosition.y += dy
+
+        // Apply the new position to the element using the CSS transform property
+        element.style.transform = `translate(${currentPosition.x}px, ${currentPosition.y}px)`
+    }
+
+    // WeakMap to store the position of each dragged element
+    const positions = new WeakMap()
+
+    // Initialize the interact.js library with the draggable element selector
+    interact(DragEl).draggable({
+        // Specify the element that triggers the drag event
+        allowFrom: targetEl,
+        // Set up event listeners for the draggable element
+        listeners: {
+            // Update the element's position when the move event is triggered
+            move(event) {
+                updatePosition(event.target, event.dx, event.dy)
+            },
+        },
+        // Set up modifiers to apply constraints to the draggable element
+        modifiers: [
+            interact.modifiers.restrictRect({
+                // Restrict the draggable element within its parent container
+                restriction: 'body',
+            }),
+        ],
+    })
+
+    $(DragEl).on('mousedown', () => {
+        $(`.draggable-panel:not(${DragEl})`).css('z-index', '100')
+        $(DragEl).css('z-index', '101')
+    })
+
+    function disableSelection(element) {
+        element.setAttribute('unselectable', 'on')
+        element.style.userSelect = 'none'
+        element.style.webkitUserSelect = 'none'
+        element.style.MozUserSelect = 'none'
+        element.style.msUserSelect = 'none'
+        element.style.OUserSelect = 'none'
+        element.onselectstart = () => false
+    }
+    let panelElements = document.querySelectorAll(
+        '.elementPanel, .layoutElementPanel, #moduleProperty, #layoutDialog, #verilogEditorPanel, .timing-diagram-panel, .testbench-manual-panel, .quick-btn'
+    )
+    panelElements.forEach((element) => {
+        disableSelection(element)
+    })
+}
+
 export function setupPanels() {
-    $('#dragQPanel')
-        .on('mousedown', () =>
-            $('.quick-btn').draggable({
-                disabled: false,
-                containment: 'window',
-            })
-        )
-        .on('mouseup', () => $('.quick-btn').draggable({ disabled: true }))
+    // $('#dragQPanel')
+    //     .on('mousedown', () =>
+    //         $('.quick-btn').draggable({
+    //             disabled: false,
+    //             containment: 'window',
+    //         })
+    //     )
+    //     .on('mouseup', () => $('.quick-btn').draggable({ disabled: true }))
+
+    // let position = { x: 0, y: 0 }
+    // interact('.quick-btn').draggable({
+    //     allowFrom: '#dragQPanel',
+    //     listeners: {
+    //         move(event) {
+    //             position.x = position.x + event.dx
+    //             position.y = position.y + event.dy
+    //             event.target.style.transform = `translate(${position.x}px, ${position.y}px)`
+    //         },
+    //     },
+    // })
+
+    dragging('#dragQPanel', '.quick-btn')
 
     setupPanelListeners('.elementPanel')
     setupPanelListeners('.layoutElementPanel')
@@ -759,19 +848,29 @@ function setupPanelListeners(panelSelector) {
     var minimizeSelector = `${panelSelector} .minimize`
     var maximizeSelector = `${panelSelector} .maximize`
     var bodySelector = `${panelSelector} > .panel-body`
+
+    dragging(headerSelector, panelSelector)
+    // let position = { x: 0, y: 0 }
     // Drag Start
-    $(headerSelector).on('mousedown', () =>
-        $(panelSelector).draggable({ disabled: false, containment: 'window' })
-    )
-    // Drag End
-    $(headerSelector).on('mouseup', () =>
-        $(panelSelector).draggable({ disabled: true })
-    )
+    // $(headerSelector).on('mousedown', () =>
+    // $(panelSelector).draggable({ disabled: false, containment: 'window' })
+    // interact(panelSelector).draggable({
+    //     allowFrom: headerSelector,
+    //     listeners: {
+    //         move(event) {
+    //             position.x += event.dx
+    //             position.y += event.dy
+
+    //             event.target.style.transform = `translate(${position.x}px, ${position.y}px)`
+    //         },
+    //     },
+    // })
+    // )
+    // // Drag End
+    // $(headerSelector).on('mouseup', () =>
+    //     $(panelSelector).draggable({ disabled: true })
+    // )
     // Current Panel on Top
-    $(panelSelector).on('mousedown', () => {
-        $(`.draggable-panel:not(${panelSelector})`).css('z-index', '70')
-        $(panelSelector).css('z-index', '71')
-    })
     var minimized = false
     $(headerSelector).on('dblclick', () =>
         minimized
